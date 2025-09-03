b287c1215c02671ee52a49ffd6a9e6116f1968be
sitaramgit <sitaramkudireddy@gmail.com>
2025-09-02 19:17:49 -0700

    Updates

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = "my-app-cache-v1";
+const urlsToCache = ["/", "/index.html"];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
